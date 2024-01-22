99eadbecb11a055b51c88c498a127f91ad1bdbfd
FifthDimensionDev <66546873+esunvoteb@users.noreply.github.com>
2024-01-22 14:43:00 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='conflumb',         # How you named your package folder (MyLib)
-    version='0.0.1',      # Start with a small number and increase it with every change you make
+    version='0.1.1',      # Start with a small number and increase it with every change you make
     license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description='Convert markdown document to confluence page',   # Give a short description about your library
     author='esunvoteb',                   # Type in your name
